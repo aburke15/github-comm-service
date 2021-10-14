8e36da644e3ad4546f5a0344fda4b1778eb1cfe2
Andre Burke <aburke.dev@gmail.com>
2021-10-13 19:15:34 -0600

    added github repo controller

---
+++ Controllers/GitHubRepositoryController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubCommunicationService.Controllers
+{
+    [ApiController]
+    [Route("api/repos")]
+    public class GitHubRepositoryController : ControllerBase
+    {
+        public GitHubRepositoryController()
+        {
+            
+        }
+
+        
+    }
+}
