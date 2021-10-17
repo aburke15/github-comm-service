8e308831882e684de9460eadf195cd5d560ceac9
Andre Burke <aburke.dev@gmail.com>
2021-10-16 19:17:17 -0600

    added get user repos method to github service

--- Services/Interfaces/IGitHubService.cs
+++ Services/Interfaces/IGitHubService.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace GitHubCommunicationService.Services.Interfaces
 {
     public interface IGitHubService
     {
+        Task<IEnumerable<object>> GetUserRepositoriesAsync(string token, string username, CancellationToken ct = default);
     }
 }
