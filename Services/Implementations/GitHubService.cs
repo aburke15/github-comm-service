5a784361c7d7098863946eebadb5036087b3940d
Andre Burke <aburke.dev@gmail.com>
2021-10-17 13:13:17 -0600

    moved rest sharp client to dependency injection

--- Services/Implementations/GitHubService.cs
+++ Services/Implementations/GitHubService.cs
@@ -17,21 +17,30 @@ namespace GitHubCommunicationService.Services.Implementations
     public class GitHubService : IGitHubService
     {
         private readonly GitHubOptions _gitHubOptions;
+        private readonly IRestClient _client;
 
-        public GitHubService(IOptions<GitHubOptions> gitHubOptions) 
-            => _gitHubOptions = Guard.Against.Null(gitHubOptions.Value, nameof(gitHubOptions));
+        public GitHubService(
+            IOptions<GitHubOptions> gitHubOptions,
+            IRestClient client)
+        {
+            _gitHubOptions = Guard.Against.Null(gitHubOptions.Value, nameof(gitHubOptions));
+            _client = Guard.Against.Null(client, nameof(client));
+        }
 
         public async Task<IEnumerable<GitHubUserRepositoryResponse>> GetUserRepositoriesAsync(
             string token, string username, CancellationToken ct = default)
         {
-            var client = new RestClient(_gitHubOptions.BaseUri) as IRestClient;
-            client.Authenticator = new JwtAuthenticator(token);
+            token = Guard.Against.NullOrWhiteSpace(token, nameof(token));
+            username = Guard.Against.NullOrWhiteSpace(username, nameof(username));
+
+            _client.BaseUrl = new Uri(_gitHubOptions.BaseUri);
+            _client.Authenticator = new JwtAuthenticator(token);
 
             var request = new RestRequest(
                 $"/users/{username}/repos", Method.GET, DataFormat.Json
             ) as IRestRequest;
 
-            var response = await client.ExecuteGetAsync(request, ct);
+            var response = await _client.ExecuteGetAsync(request, ct);
 
             if (!response.IsSuccessful)
                 return Enumerable.Empty<GitHubUserRepositoryResponse>();
--- Workers/GitHubRepositoryBackgroundService.cs
+++ Workers/GitHubRepositoryBackgroundService.cs
@@ -10,17 +10,17 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace GitHubCommunicationService.Services.Implementations
+namespace GitHubCommunicationService.Workers
 {
-    public class GitHubRepoBackgroundService : BackgroundService
+    public class GitHubRepositoryBackgroundService : BackgroundService
     {
         private readonly IServiceProvider _services;
-        private readonly ILogger<GitHubRepoBackgroundService> _logger;
+        private readonly ILogger<GitHubRepositoryBackgroundService> _logger;
         private readonly GitHubOptions _gitHubOptions;
 
-        public GitHubRepoBackgroundService(
+        public GitHubRepositoryBackgroundService(
             IServiceProvider services, 
-            ILogger<GitHubRepoBackgroundService> logger,
+            ILogger<GitHubRepositoryBackgroundService> logger,
             IOptions<GitHubOptions> gitHubOptions)
         {
             _services = Guard.Against.Null(services, nameof(services));
@@ -40,12 +40,12 @@ namespace GitHubCommunicationService.Services.Implementations
                     var _gitHubService = scope.ServiceProvider
                         .GetRequiredService<IGitHubService>();
 
-                    var resutls = await _gitHubService.GetUserRepositoriesAsync(
+                    var results = await _gitHubService.GetUserRepositoriesAsync(
                         token: _gitHubOptions.AuthToken, 
                         username: _gitHubOptions.Username, 
                         ct: stoppingToken);
-
-                    Console.WriteLine(resutls);
+                    // TODO: persist to database
+                    Console.WriteLine(results);
 
                     Console.WriteLine($"Doing work: {count} at - [{DateTime.Now}]");
                     count++;
@@ -53,15 +53,16 @@ namespace GitHubCommunicationService.Services.Implementations
                 catch (Exception ex)
                 {
                     _logger.LogError(
-                        $"Error encountered on server. Background Service {nameof(GitHubRepoBackgroundService)} - {ex.Message}"
+                        $"Error encountered on server. Background Service {nameof(GitHubRepositoryBackgroundService)} - {ex.Message}"
                     );
                 }
 
+                // TODO: change to TimeSpan.From(time in options file) 
                 await Task.Delay(5000, stoppingToken);
             }
 
             _logger.LogInformation(
-                $"Background Service {nameof(GitHubRepoBackgroundService)} has stopped at : [{DateTime.Now}]"
+                $"Background Service {nameof(GitHubRepositoryBackgroundService)} has stopped at : [{DateTime.Now}]"
             );
         }
     }
