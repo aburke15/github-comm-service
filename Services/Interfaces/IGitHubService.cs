11c237cce1725df8a103ebdcd2e575da49b7282a
Andre Burke <aburke.dev@gmail.com>
2021-10-17 01:01:53 -0600

    added response for github user repo request

--- Services/Interfaces/IGitHubService.cs
+++ Services/Interfaces/IGitHubService.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using GitHubCommunicationService.Responses;
 
 namespace GitHubCommunicationService.Services.Interfaces
 {
     public interface IGitHubService
     {
-        Task<IEnumerable<object>> GetUserRepositoriesAsync(string token, string username, CancellationToken ct = default);
+        Task<IEnumerable<GitHubUserRepositoryResponse>> GetUserRepositoriesAsync(string token, string username, CancellationToken ct = default);
     }
 }
