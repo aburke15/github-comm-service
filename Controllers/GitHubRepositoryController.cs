ce52356144acd78535c638540fed4a299aea8e5e
Andre Burke <aburke.dev@gmail.com>
2021-10-24 22:27:36 -0600

    enabled the ability to add a connection string from the command line

--- Controllers/GitHubRepositoryController.cs
+++ Controllers/GitHubRepositoryController.cs
@@ -1,4 +1,7 @@
+using System.Threading;
 using System.Threading.Tasks;
+using GitHubCommunicationService.Abstractions;
+using GitHubCommunicationService.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GitHubCommunicationService.Controllers
@@ -7,16 +10,25 @@ namespace GitHubCommunicationService.Controllers
     [Route("api/repos")]
     public class GitHubRepositoryController : ControllerBase
     {
-        public GitHubRepositoryController()
+        private readonly IGitHubService _gitHubService;
+        
+        public GitHubRepositoryController(IGitHubService gitHubService)
         {
-            
+            _gitHubService = gitHubService;
         }
-
+        
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             await Task.Delay(5000);
             return Ok();
+        }
+
+        [HttpGet("tests")]
+        public async Task<IActionResult> GetTestData(CancellationToken ct)
+        {
+            var (databaseName, collectionName) = ("restaurantHoster", "Reservations");
+            return Ok(await _gitHubService.GetAllUserRepositoriesFromDbAsync(databaseName, collectionName, ct));
         }
     }
 }
