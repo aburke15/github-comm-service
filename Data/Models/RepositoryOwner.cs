732f647b4d2055355a2e237a125f0b15b3346084
Andre Burke <aburke.dev@gmail.com>
2021-11-01 23:57:58 -0600

    added repository records

---
+++ Data/Models/RepositoryOwner.cs
@@ -0,0 +1,44 @@
+using System;
+using Newtonsoft.Json;
+
+namespace GitHubCommunicationService.Data.Models
+{
+    public record RepositoryOwner
+    {
+        [JsonProperty("login")] public string? Login { get; init; }
+
+        [JsonProperty("id")] public long Id { get; init; }
+
+        [JsonProperty("node_id")] public string? NodeId { get; init; }
+
+        [JsonProperty("avatar_url")] public Uri? AvatarUrl { get; init; }
+
+        [JsonProperty("gravatar_id")] public string? GravatarId { get; init; }
+
+        [JsonProperty("url")] public Uri? Url { get; init; }
+
+        [JsonProperty("html_url")] public Uri? HtmlUrl { get; init; }
+
+        [JsonProperty("followers_url")] public Uri? FollowersUrl { get; init; }
+
+        [JsonProperty("following_url")] public string? FollowingUrl { get; init; }
+
+        [JsonProperty("gists_url")] public string? GistsUrl { get; init; }
+
+        [JsonProperty("starred_url")] public string? StarredUrl { get; init; }
+
+        [JsonProperty("subscriptions_url")] public Uri? SubscriptionsUrl { get; init; }
+
+        [JsonProperty("organizations_url")] public Uri? OrganizationsUrl { get; init; }
+
+        [JsonProperty("repos_url")] public Uri? ReposUrl { get; init; }
+
+        [JsonProperty("events_url")] public string? EventsUrl { get; init; }
+
+        [JsonProperty("received_events_url")] public Uri? ReceivedEventsUrl { get; init; }
+
+        [JsonProperty("type")] public string? Type { get; init; }
+
+        [JsonProperty("site_admin")] public bool SiteAdmin { get; init; }
+    }
+}
---
+++ Data/Models/RepositoryPermissions.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+
+namespace GitHubCommunicationService.Data.Models
+{
+    public record RepositoryPermissions
+    {
+        [JsonProperty("admin")] public bool Admin { get; init; }
+
+        [JsonProperty("maintain")] public bool Maintain { get; init; }
+
+        [JsonProperty("push")] public bool Push { get; init; }
+
+        [JsonProperty("triage")] public bool Triage { get; init; }
+
+        [JsonProperty("pull")] public bool Pull { get; init; }
+    }
+}
