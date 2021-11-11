6fffcbf4f2209555436fd32f4be08d4fed41b5c8
Andre Burke <aburke.dev@gmail.com>
2021-11-10 20:14:48 -0700

    two packages updated to target net6.0

--- Services/GitHubApiService.cs
+++ Services/GitHubApiService.cs
@@ -19,35 +19,34 @@ using Newtonsoft.Json;
 using RestSharp;
 using RestSharp.Authenticators;
 
-namespace GitHubCommunicationService.Services
+namespace GitHubCommunicationService.Services;
+
+public class GitHubApiService : IGitHubApiService
 {
-    public class GitHubApiService : IGitHubApiService
+    private readonly IGitHubApiClient _gitHubApiClient;
+    private readonly IMapper _mapper;
+
+    public GitHubApiService(
+        IGitHubApiClient gitHubApiClient,
+        IMapper mapper)
     {
-        private readonly IGitHubApiClient _gitHubApiClient;
-        private readonly IMapper _mapper;
+        _gitHubApiClient = Guard.Against.Null(gitHubApiClient, nameof(gitHubApiClient));
+        _mapper = Guard.Against.Null(mapper, nameof(mapper));
+    }
 
-        public GitHubApiService(
-            IGitHubApiClient gitHubApiClient,
-            IMapper mapper)
-        {
-            _gitHubApiClient = Guard.Against.Null(gitHubApiClient, nameof(gitHubApiClient));
-            _mapper = Guard.Against.Null(mapper, nameof(mapper));
-        }
+    public async Task<IReadOnlyList<Repository>> GetUserRepositoriesFromApiAsync(CancellationToken ct = default)
+    {
+        var result = await _gitHubApiClient.GetRepositoriesForUserAsync(ct);
 
-        public async Task<IReadOnlyList<Repository>> GetUserRepositoriesFromApiAsync(CancellationToken ct = default)
-        {
-            var result = await _gitHubApiClient.GetRepositoriesForUserAsync(ct);
-            
-            if (!result.IsSuccessful)
-                throw new Exception($"An error occured: {result.Message}");
+        if (!result.IsSuccessful)
+            throw new Exception($"An error occured: {result.Message}");
 
-            result = Guard.Against.Null(result, nameof(result));
-            var json = result.Json;
+        result = Guard.Against.Null(result, nameof(result));
+        var json = result.Json;
 
-            if (string.IsNullOrWhiteSpace(json))
-                return new List<Repository>();
-            
-            return JsonConvert.DeserializeObject<IReadOnlyList<Repository>>(json)!;
-        }
+        if (string.IsNullOrWhiteSpace(json))
+            return new List<Repository>();
+
+        return JsonConvert.DeserializeObject<IReadOnlyList<Repository>>(json)!;
     }
 }
